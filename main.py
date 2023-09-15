922b174d11c6aeb376906890e0e5ecfdce5f68a3
Govindan-murugesan <govindan.murugesan@photoninfotech.net>
2023-09-15 22:40:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+# print("I love learning python")
+# print("It's really nice")
+
+#Variables
+
+#Strings
+# First_name = 'Govindan'
+# Second_name = "Murugesan"
+# Full_name = First_name + Second_name
+# print("Full Name is:" + Full_name)
+# print(type(Full_name))
+
+#int
+# age = 33
+# age += 1
+# print(age)
+# print("Your age is: " + str(age))
+# print(type(age))
+
+#float
+# height = 175.6
+# print(height)
+# print("Your height is: " + str(height) + " cm")
+# print(type(height))
+
+# #boolean
+# human = True
+# print(human)
+# print("Are you a human: " + str(human))
+# print(type(human))
+
+
+#Multiple Assignment
+#
+# name = "Govind"
+# age = 33
+# attractive = False
+
+# name, age, attractive = "Govind", 33, False
+#
+# print(name)
+# print(age)
+# print(attractive)
+
+# Dhoni = 300
+# Virat = 300
+# Shewag = 300
+
+# Dhoni = Virat = Shewag = 300
+#
+# print(Dhoni)
+# print(Virat)
+# print(Shewag)
+
+#String methods
+
+# name = "govinds"
+# print(len(name))
+# print(name.find("o"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isnumeric())
+# print(name.isalpha())
+# print(name.count(name))
+# print(name*2)
+
+
+# #TypeCasting -> Converting the dtat type of the value to another data type
+# x=1 #int
+# y=2.4 #float
+# z="3" #str
+#
+# x=str(x)
+# y=str(y)
+#
+# print(x)
+# print(y)
+# print(z)
+
+
+name = input("What is your name?: ")
+print("Hello " + name + "!")
+
+
